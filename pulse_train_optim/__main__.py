* add correlation loss; * fix fa input calculation;

pulse_train_optim/__main__.py
@@ -43,13 +43,13 @@ def configure(sim_params: eso.SimulationParameters, optim_config: poo.ConfigOpti
 def build_input_tensors(pulse_fa: torch.tensor):
     # want the ETL length optimizing params to be between 0 and 1
     # and enforce bounds here
-    fa_bounds = torch.tensor([100.0, 180.0])
+    # fa_bounds = torch.tensor([100.0, 180.0])
     # use sigmoid to map to between 0 and 1
-    mapped_pulse_fa = torch.nn.Sigmoid()(pulse_fa)
+    # mapped_pulse_fa = torch.nn.Sigmoid()(pulse_fa)
     # add lower bound and scale with range
-    mapped_pulse_fa *= torch.gradient(fa_bounds)[0][0]
-    mapped_pulse_fa += fa_bounds[0]
-    return mapped_pulse_fa
+    # mapped_pulse_fa *= torch.gradient(fa_bounds)[0][0]
+    # mapped_pulse_fa += fa_bounds[0]
+    return torch.nn.Sigmoid()(pulse_fa)
 
 
 def main(sim_params: eso.SimulationParameters, optim_config: poo.ConfigOptimization):
@@ -66,14 +66,17 @@ def main(sim_params: eso.SimulationParameters, optim_config: poo.ConfigOptimizat
         gp.set_device(device)
     acquisition.set_device(device)
 
-    # need to set initial values
+    # need to set initial values - take fa to be between 0 and 1. map it with a sigmoid to ensure bounds.
+    # 1 is 180 degrees
     fa = torch.ones((sim_params.sequence.ETL,), device=device, requires_grad=True)
 
     # set optimizer
     optimizer = torch.optim.SGD([fa], lr=optim_config.lr, momentum=optim_config.momentum)
     torch.autograd.set_detect_anomaly(True)
     # set loss obj
-    loss = pto.LossOptimize(lambda_snr=1.0, lambda_corr=1.0)
+    loss = pto.LossOptimize(lambda_snr=0.5, lambda_corr=1.0)
+    sim_data = eso.SimulationData.from_sim_parameters(sim_params=sim_params, device=device)
+    loss.loss_corr.set_weight_matrix(sim_data=sim_data)
     # run through optimization
     with tqdm.auto.trange(optim_config.num_steps) as t:
         t.set_description(f"progress")
@@ -96,7 +99,12 @@ def main(sim_params: eso.SimulationParameters, optim_config: poo.ConfigOptimizat
             optimizer.step()
             t.set_postfix(ordered_dict=OrderedDict({"loss": loss.value.item()}))
 
-    logging.info(f"optimized fa train: {build_input_tensors(fa)}")
+    optim_fa = build_input_tensors(fa)
+    logging.info(f"optimized fa train: {optim_fa}")
+    file_name = f"{optim_config.optim_save_path.stem}_optimized_fa"
+    save_name = optim_config.optim_save_path.with_name(file_name).with_suffix(".pt")
+    logging.info(f"saving file: {save_name}")
+    torch.save(optim_fa, save_name.as_posix())
 
 
 if __name__ == '__main__':

pulse_train_optim/optimizer.py
@@ -1,3 +1,5 @@
+import itertools
+
 import torch
 from emc_sim import options
 
@@ -29,9 +31,47 @@ class LossSNR(LossBC):
 class LossCorr(LossBC):
     def __init__(self, emphasis: float = 1.0):
         super().__init__(name="LossCorr", emphasis=emphasis)
+        self.weight_matrix: torch.tensor = NotImplementedError
+
+    def set_weight_matrix(self, sim_data: options.SimulationData):
+        # t1 gets avd out. t2 b1 get combined:
+        param_list = [(t2, b1) for t2 in sim_data.t2_vals for b1 in sim_data.b1_vals]
+        num_param_pairs = param_list.__len__()
+        weight_matrix = torch.tril(torch.ones((num_param_pairs, num_param_pairs)), -1)
+        # set t2 inter dependencies
+        for t2 in sim_data.t2_vals:
+            t2_idxs = []
+            for p_idx in range(num_param_pairs):
+                t2p, _ = param_list[p_idx]
+                if t2p == t2:
+                    t2_idxs.append(p_idx)
+
+            for o1_idx, o2_idx in itertools.product(t2_idxs, t2_idxs):
+                weight_matrix[o1_idx,o2_idx] = 0.0
+        self.weight_matrix = weight_matrix.to(sim_data.device)
 
     def calculate_loss(self, sim_data: options.SimulationData):
-        self.value = -1
+        # want to minimize cross correlation across b1 and t2 values
+        emc_mag = sim_data.emc_signal_mag
+        emc_phase = sim_data.emc_signal_phase
+        # digress T1 out    dims: [t1, t2, b1, echos]
+        while emc_mag.shape.__len__() > 3:
+            emc_mag = torch.mean(emc_mag, dim=0)
+        while emc_phase.shape.__len__() > 3:
+            emc_phase = torch.mean(emc_phase, dim=0)
+        # flatten matrix
+        emc_mag = torch.reshape(emc_mag, (-1, emc_mag.shape[-1]))
+        emc_phase = torch.reshape(emc_phase, (-1, emc_phase.shape[-1]))
+        # normalize
+        norm = torch.norm(emc_mag, dim=-1, keepdim=True)
+        emc_mag = torch.div(emc_mag, norm)
+        # dims [t2b1, echoes]
+        mag_source = emc_mag[:, None, :] - emc_mag
+        loss_matrix_mag = torch.einsum("ijk, ijk -> ij", mag_source, mag_source)
+        phase_source = emc_phase[:, None, :] - emc_phase
+        loss_matrix_phase = torch.einsum("ijk, ijk -> ij", phase_source, phase_source)
+        loss = self.weight_matrix * loss_matrix_mag + 0.2 * self.weight_matrix * loss_matrix_phase
+        self.value = torch.sum(loss) / emc_mag.shape[0]
 
 
 class LossOptimize: