25336efd985af796446a0e68e04a61110a1ca680
Jochen Schmidt <jschmidt@cbs.mpg.de>
2023-10-18 13:20:39 +0200

    * switch to pypsi;

--- emc_sim/__init__.py
+++ emc_sim/__init__.py
@@ -1,7 +1,7 @@
 # make sure all sources are seen -> submodule
 import sys
 import pathlib
-pulse_path = pathlib.Path(__file__).absolute().parent.parent.joinpath("rf_pulse_files/")
+pulse_path = pathlib.Path(__file__).absolute().parent.parent.joinpath("pypulseq_interface/")
 sys.path.append(pulse_path.as_posix())
 
 # explicitely state available functions
