8010cb4e61cfa970a71044154f7e9432c13aac55
Jochen Schmidt <jschmidt@cbs.mpg.de>
2023-11-06 16:51:30 +0100

    * minor fix elif;

--- emc_sim/simulations/sim.py
+++ emc_sim/simulations/sim.py
@@ -8,9 +8,9 @@ log_module = logging.getLogger(__name__)
 def simulate(sim_params: options.SimulationParameters, device: torch.device = torch.device("cpu")):
     if sim_params.config.sim_type == "mese":
         sim_obj = simulations.MESE(sim_params=sim_params, device=device)
-    if sim_params.config.sim_type == "megesse":
+    elif sim_params.config.sim_type == "megesse":
         sim_obj = simulations.MEGESSE(sim_params=sim_params, device=device)
-    if sim_params.config.sim_type == "fid":
+    elif sim_params.config.sim_type == "fid":
         sim_obj = simulations.FID(sim_params=sim_params, device=device)
     # if sim_params.config.sim_type == "single":
     #     sim_obj = simulations.(sim_params=sim_params, device=device)
