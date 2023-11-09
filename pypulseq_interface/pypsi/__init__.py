05e54fd749bb126857b90fd155d4e22a5cd093cb
schmidt-jo <jochen.schmidt@tuta.io>
2023-11-09 15:33:51 +0100

    fix submodule url

---
+++ pypulseq_interface/pypsi/__init__.py
@@ -0,0 +1,3 @@
+from .config import Params
+from . import parameters
+
---
+++ pypulseq_interface/pypsi/parameters/__init__.py
@@ -0,0 +1,7 @@
+
+from .emc_params import EmcParameters
+from .pypulseq_params import PypulseqParameters
+from .recon_params import ReconParameters
+from .sampling_k_traj_params import SamplingKTrajectoryParameters
+from .scanner_params import ScannerParameters
+from .rf_params import RFParameters
