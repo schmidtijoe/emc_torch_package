902a4507f8e0d514caa9529dc753445b56afe226
schmidt-jo <jochen.schmidt@tuta.io>
2023-11-16 16:44:28 +0100

    * add autodmri

--- src/emc_torch/__init__.py
+++ src/emc_torch/__init__.py
@@ -6,6 +6,8 @@ __all__ = ["plotting", "options", "DB"]
 # make sure all sources are seen -> submodule
 import sys
 import pathlib
-pulse_path = pathlib.Path(__file__).absolute().parent.parent.joinpath("pypulseq_interface/")
-sys.path.append(pulse_path.as_posix())
+path_content_root = pathlib.Path(__file__).absolute().parent.parent
+pulse_path = path_content_root.joinpath("pypulseq_interface/")
+dmri_path = path_content_root.joinpath("autodmri")
 
+sys.path.append(pulse_path.as_posix())
