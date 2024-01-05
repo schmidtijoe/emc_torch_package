* minor fixes; * add 0 signal handling to fit; * add pd & d scaling based on histograms

src/emc_torch/db_class.py
@@ -46,11 +46,12 @@ class DB:
 
     def plot(self,
              out_path: plib.Path | str, name: str = "",
-             t1_range_s: tuple = None, t2_range_ms: tuple = (20, 50), b1_range: tuple = (0.5, 1.2)):
+             t1_range_s: tuple = None, t2_range_ms: tuple = (20, 50), b1_range: tuple = (0.5, 1.2),
+             format: str = "html"):
         if name:
             name = f"_{name}"
         # select range
-        df = self.pd_dataframe
+        df = self.pd_dataframe.copy()
         df["t2"] = 1e3 * df["t2"]
         df["t2"] = df["t2"].round(2)
         df["b1"] = df["b1"].round(2)
@@ -64,8 +65,7 @@ class DB:
         # for now we only take one t1 value
         df = df[df["t1"] == df["t1"].unique()[0]].drop(columns=["t1"]).drop(columns="index").reset_index(drop=True)
         # setup colorscales to use
-        x = np.linspace(0.2, 1, len(df["t2"].unique()))
-        c_scales = ["Purples", "Oranges", "Greens", "Reds", "Blues"]
+        c_scales = ["Purples", "Oranges", "Greens", "Blues"]
         echo_ax = df["echo"].to_numpy()
         # setup subplots
         while len(df["b1"].unique()) > len(c_scales):
@@ -75,7 +75,7 @@ class DB:
             b1_vals.pop(drop_idx)
             df = df[df["b1"].isin(b1_vals)]
         # setup subplots
-        while len(df["t2"].unique()) > 20:
+        while len(df["t2"].unique()) > 12:
             # drop every second t2 value
             t2_vals = df["t2"].unique().tolist()[::2]
             df = df[df["t2"].isin(t2_vals)]
@@ -84,6 +84,7 @@ class DB:
         fig = psub.make_subplots(
             2, 1, shared_xaxes=True, subplot_titles=titles
         )
+        x = np.linspace(0.2, 1, len(df["t2"].unique()))
         # edit axis labels
         fig['layout']['xaxis2']['title'] = 'Echo Number'
         fig['layout']['yaxis']['title'] = 'Signal [a.u.]'
@@ -99,7 +100,7 @@ class DB:
                 mag = temp_df[temp_df["t2"] == t2]["emc_mag"].to_numpy()
                 mag /= np.abs(np.max(mag))
                 fig.add_trace(
-                    go.Scatter(
+                    go.Scattergl(
                         x=echo_ax, y=mag, marker_color=c, showlegend=False
                     ),
                     1, 1
@@ -107,13 +108,17 @@ class DB:
 
                 phase = temp_df[temp_df["t2"] == t2]["emc_phase"].to_numpy()
                 fig.add_trace(
-                    go.Scatter(
+                    go.Scattergl(
                         x=echo_ax, y=phase, marker_color=c, showlegend=False
                     ),
                     2, 1
                 )
+            if b1_idx == num_plot_b1s-1:
+                showticks = True
+            else:
+                showticks = False
             # add colorbar
-            colorbar_trace = go.Scatter(
+            colorbar_trace = go.Scattergl(
                 x=[None], y=[None], mode='markers',
                 showlegend=False,
                 marker=dict(
@@ -121,8 +126,9 @@ class DB:
                     cmin=t2_range_ms[0], cmax=t2_range_ms[1],
                     colorbar=dict(
                         title=f"{df['b1'].unique()[b1_idx]}",
-                        x=1.02 + 0.05 * b1_idx
-                    )
+                        x=1.02 + 0.05 * b1_idx,
+                        showticklabels=showticks
+                    ),
                 )
             )
             fig.add_trace(colorbar_trace, 1, 1)
@@ -138,9 +144,18 @@ class DB:
         )
 
         out_path = plib.Path(out_path).absolute()
-        fig_file = out_path.joinpath(f"emc_db{name}").with_suffix(".html")
-        log_module.info(f"writing file: {fig_file.as_posix()}")
-        fig.write_html(fig_file.as_posix())
+        if format == "html":
+            fig_file = out_path.joinpath(f"emc_db{name}").with_suffix(".html")
+            log_module.info(f"writing file: {fig_file.as_posix()}")
+            fig.write_html(fig_file.as_posix())
+        elif format in ["pdf", "svg", "png"]:
+            fig_file = out_path.joinpath(f"emc_db{name}").with_suffix(f".{format}")
+            log_module.info(f"writing file: {fig_file.as_posix()}")
+            fig.write_image(fig_file.as_posix(), width=1200, height=800)
+        else:
+            err = f"Format {format} not recognized"
+            log_module.error(err)
+            raise AttributeError(err)
 
     def save(self, path: typing.Union[str, plib.Path]):
         path = plib.Path(path).absolute()

src/fit.py
@@ -19,7 +19,10 @@ def main(fit_config: options.FitConfig):
     if fit_config.save_name_prefix:
         fit_config.save_name_prefix += f"_"
     in_path = plib.Path(fit_config.nii_path).absolute()
-    name = f"{fit_config.save_name_prefix}{in_path.stem}"
+    stem = in_path.stem
+    for suffix in in_path.suffixes:
+        stem = stem.removesuffix(suffix)
+    name = f"{fit_config.save_name_prefix}{stem}"
 
     log_module.info("__ Load data")
     data_nii, db, b1_nii, data_affine, b1_affine = io.load_data(fit_config=fit_config)
@@ -43,6 +46,7 @@ def main(fit_config: options.FitConfig):
     db_torch_norm = torch.squeeze(torch.from_numpy(db_norm).to(device))
     batch_size = 3000
     nii_idxs = torch.split(torch.arange(nii_data.shape[0]), batch_size)
+    nii_zero = torch.sum(torch.abs(nii_data), dim=-1) < 1e-6
     nii_data = torch.split(nii_data, batch_size, dim=0)
     # make scaling map
     t2_vals = torch.from_numpy(db.pd_dataframe[db.pd_dataframe["echo"] == 1]["t2"].values).to(device)
@@ -51,7 +55,9 @@ def main(fit_config: options.FitConfig):
     # b1 penalty of b1 database vs b1 input
     # dim b1 [xyz], db b1 - values for each entry [t1 t2 b1]
     if b1_nii is not None:
-        b1_nii = torch.reshape(b1_nii / 100, (num_flat_dim,)).to(device)
+        b1_nii = torch.reshape(b1_nii, (num_flat_dim,)).to(device)
+        if torch.max(b1_nii) > 10:
+            b1_nii = b1_nii / 100
         b1_nii = torch.split(b1_nii, batch_size, dim=0)
         use_b1 = True
         b1_weight = fit_config.b1_weight
@@ -87,13 +93,20 @@ def main(fit_config: options.FitConfig):
         t2[nii_idxs[idx]] = t2_vals[min_idx]
         b1[nii_idxs[idx]] = b1_vals[min_idx]
         s0[nii_idxs[idx]] = db_torch_norm[min_idx]
-
+    # set t2 0 for signal 0 (eg. for bet) We could in principle use this to reduce computation demands
+    # by not needing to compute those entries,
+    # however we want to estimate the b1
+    t2[nii_zero] = 0.0
     # reshape
+    if torch.max(t2) < 5:
+        # cast to ms
+        t2 = 1e3 * t2
     t2 = torch.reshape(t2, data_nii_input_shape[:-1])
     r2 = torch.nan_to_num(1000.0 / t2, nan=0.0, posinf=0.0)
     t2 = t2.numpy(force=True)
     r2 = r2.numpy(force=True)
     b1 = torch.reshape(b1, data_nii_input_shape[:-1]).numpy(force=True)
+    s0[nii_zero] = 0.0
     s0 = torch.reshape(s0, data_nii_input_shape[:-1]).cpu()
     pd_like = torch.nan_to_num(
         torch.divide(
@@ -102,10 +115,20 @@ def main(fit_config: options.FitConfig):
         ),
         nan=0.0, posinf=0.0
     )
-    double_weighting = (torch.squeeze(nii_scale) * torch.squeeze(s0)).numpy(force=True)
-    # arbitrarily scale to 1000: scale to 200 000 and keep a fraction (some low norm values explode)
-    # pd_like = torch.clip(torch.divide(10 * torch.mean(pd_like), torch.max(pd_like)), 0.0, 1e3).numpy(force=True)
-    pd_like = pd_like.numpy(force=True)
+    double_weighting = torch.squeeze(nii_scale) * torch.squeeze(s0)
+    # we want to calculate histograms for both, and find upper cutoffs of the data values based on the histograms
+    # since both might explode
+    dw_hist, dw_bins = torch.histogram(double_weighting.flatten(), bins=200)
+    pd_hist, pd_bins = torch.histogram(pd_like.flatten(), bins=200)
+    # find percentage where 99.5 % of data lie
+    dw_hist_perc = torch.cumsum(dw_hist, dim=0) / torch.sum(dw_hist, dim=0)
+    pd_hist_perc = torch.cumsum(pd_hist, dim=0) / torch.sum(pd_hist, dim=0)
+
+    dw_cutoff_value = dw_bins[torch.nonzero(dw_hist_perc > 0.995)[0].item()]
+    pd_cutoff_value = pd_bins[torch.nonzero(pd_hist_perc > 0.995)[0].item()]
+
+    double_weighting = torch.clamp(double_weighting, min=0.0, max=dw_cutoff_value).numpy(force=True)
+    pd_like = torch.clamp(pd_like, min=0.0, max=pd_cutoff_value).numpy(force=True)
 
     # save
     names = [f"t2", f"r2", f"b1", f"pd_like_scaling", "d_norm_w"]