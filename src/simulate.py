6620a1ebacb0a5832541c63c596a5762e88bf924
schmidt-jo <jochen.schmidt@tuta.io>
2024-04-11 12:47:45 +0200

    * implement sim function call;

--- src/simulate.py
+++ src/simulate.py
@@ -5,8 +5,38 @@ log_module = logging.getLogger(__name__)
 logging.getLogger('simple_parsing').setLevel(logging.WARNING)
 
 
-def main(sim_params: options.SimulationParameters):
+def sim(
+        emc_seq_file: str = "", pypsi_path: str = "", pulse_file: str = "",
+        sim_type: str = "megesse",
+        resample_pulse_to_dt_us: float = 5.0,
+        use_gpu: bool = False, gpu_device: int = 0,
+        visualize: bool = False, debug: bool = False,
+        sample_number: int = 1000, length_z: float = 0.005,
+        t2_list: list = None, b1_list: list = None) -> db_class.DB:
+    """
+    Function to be called when using simulation aspect of package from another python application.
+    I.E. optimization or ai model training.
+    basically just create the parameter options from kwargs and passing it onto core
+    """
+    config = options.SimulationConfig(
+        pypsi_path=pypsi_path, emc_seq_file=emc_seq_file, pulse_file=pulse_file,
+        save_path="", database_name="_", sim_type=sim_type, signal_fourier_sampling=False,
+        visualize=visualize, debug=debug, resample_pulse_to_dt_us=resample_pulse_to_dt_us,
+        use_gpu=use_gpu, gpu_device=gpu_device
+    )
+    settings = options.SimulationSettings(
+        sample_number=sample_number, length_z=length_z,
+        t1_list=[1.5], t2_list=t2_list, b1_list=b1_list
+    )
+    sim_params = options.SimulationParameters(config=config, settings=settings)
+    db = core_sim(sim_params=sim_params)
+    return db
 
+
+def core_sim(sim_params: options.SimulationParameters) -> db_class.DB:
+    """
+    core simulation and plotting
+    """
     sim_params.config.display()
 
     if sim_params.config.sim_type.startswith("mese"):
@@ -34,7 +64,15 @@ def main(sim_params: options.SimulationParameters):
             sim_obj.plot_signal_traces()
         # plot database
         db.plot(sim_obj.fig_path)
+    return db
 
+
+def cli_sim(sim_params: options.SimulationParameters):
+    """
+    Function to be called when using tool as cmd line interface, passing the cli created options.
+    Just doing the core sim and saving the data
+    """
+    db = core_sim(sim_params=sim_params)
     # save files
     save_path = plib.Path(sim_params.config.save_path).absolute()
     if sim_params.config.config_file:
@@ -65,7 +103,7 @@ if __name__ == '__main__':
         logging.getLogger().setLevel(logging.DEBUG)
 
     try:
-        main(sim_params=opts)
+        cli_sim(sim_params=opts)
 
     except Exception as e:
         logging.exception(e)
